71b762ef1af3d1c36b4ca57085cce9df35498534
chemabrunofbmoll <56310308+chemabrunofbmoll@users.noreply.github.com>
2020-05-20 16:57:13 +0200

    Subida después de llamada meet

---
+++ TODOSentencias.java
@@ -0,0 +1,138 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RentaCar;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author victoriapenas & josemariahernandez
+ * @version 1.0
+ * @since 2020-20-05
+ */
+public class TODOSentencias {
+
+    private final Conexion_BBDD con;
+    PreparedStatement ps;
+    ResultSet res;
+
+    public TODOSentencias() {
+        con = new Conexion_BBDD();
+    }
+
+    // Método insertar en BBDD
+    public boolean insertSQL(String datos[], String insert) {
+        boolean estado = false;
+        try {
+            ps = con.conectado().prepareStatement(insert);
+            for (int i = 0; i <= datos.length - 1; i++) {
+                ps.setString(i + 1, datos[i]);
+            }
+            ps.execute();
+            ps.close();
+            estado = true;
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return estado;
+    }
+
+    // Check tablas
+    public Object[][] tablas(String nombrecol[], String tabla, String sql) {
+        int registros = 0;
+
+        try {
+            ps = con.conectado().prepareStatement("select count(*) as total from " + tabla);
+            res = ps.executeQuery();
+            res.next();
+            registros = res.getInt("total");
+            res.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        Object[][] dat = new String[registros][nombrecol.length];
+        String col[] = new String[nombrecol.length];
+
+        try {
+            ps = con.conectado().prepareStatement(sql);
+            res = ps.executeQuery();
+            int i = 0;
+            while (res.next()) {
+                for (int j = 0; j <= nombrecol.length - 1; j++) {
+                    col[j] = res.getString(nombrecol[j]);
+                    dat[i][j] = col[j];
+                }
+                i++;
+            }
+            res.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return dat;
+    }
+    // Check datos en string
+
+    public String datosString(String nombre_columna, String sentenciasql) {
+
+        String datos = "";
+        try {
+            ps = con.conectado().prepareStatement(sentenciasql);
+            res = ps.executeQuery();
+            while (res.next()) {
+                datos = res.getString(nombre_columna);
+            }
+            res.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return datos;
+    }
+    // Check rellenarComboBox
+
+    public Object[] rellenarComboBox(String tabla, String nombrecol, String sql) {
+        int registros = 0;
+        try {
+            ps = con.conectado().prepareStatement("SELECT count(*) as total FROM " + tabla);
+            res = ps.executeQuery();
+            res.next();
+            registros = res.getInt("total");
+            res.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        Object[] datos = new Object[registros];
+        try {
+            ps = con.conectado().prepareStatement(sql);
+            res = ps.executeQuery();
+            int i = 0;
+            while (res.next()) {
+                datos[i] = res.getObject(nombrecol);
+                i++;
+            }
+            res.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return datos;
+    }
+    // Check existencias
+    public boolean existencias(String campo, String from_where) {
+        int registros = 0;
+        try {
+            ps = con.conectado().prepareStatement("SELECT count(" + campo + ") as total  " + from_where);
+            res = ps.executeQuery();
+            res.next();
+            registros = res.getInt("total");
+            res.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        return registros > 0;
+    }
+}
---
+++ Vehiculo.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RentaCar;
+
+/**
+ *
+ * @author Chema
+ */
+public class Vehiculo {
+    
+    // Atributos
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private double precioDia;
+    private char clase;
+    private int numeroPuertas;
+    private boolean disponible;
+    
+    // Constructor vacío
+    public Vehiculo(){
+        
+    }
+    
+    // Constructor
+    public Vehiculo(String matricula, String marca, String modelo, double precioDia, char clase, int numeroPuertas, boolean disponible) {
+        this.setMatricula(matricula);
+        this.setMarca(marca);
+        this.setModelo(modelo);
+        this.setPrecioDia(precioDia);
+        this.setClase(clase);
+        this.setNumeroPuertas(numeroPuertas);
+        this.setDisponible(disponible);
+    }
+    
+    // Constructor copia
+    public Vehiculo(Vehiculo v1) {
+        this.setMatricula(v1.getMatricula());
+        this.setMarca(v1.getMarca());
+        this.setModelo(v1.getModelo());
+        this.setPrecioDia(v1.getPrecioDia());
+        this.setClase(v1.getClase());
+        this.setNumeroPuertas(v1.getNumeroPuertas());
+        this.setDisponible(v1.isDisponible());
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPrecioDia() {
+        return precioDia;
+    }
+
+    public void setPrecioDia(double precioDia) {
+        this.precioDia = precioDia;
+    }
+
+    public char getClase() {
+        return clase;
+    }
+
+    public void setClase(char clase) {
+        this.clase = clase;
+    }
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public void setNumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+}
