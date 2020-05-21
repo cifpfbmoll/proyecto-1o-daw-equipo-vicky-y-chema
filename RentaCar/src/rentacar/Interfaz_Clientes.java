88b52d077952b2af45bc9459f05bdc6301a16a8c
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2020-05-21 12:38:09 +0200

    test pete

--- RentaCar/src/rentacar/Interfaz_Clientes.java
+++ RentaCar/src/rentacar/Interfaz_Clientes.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package rentacar;
+package RentaCar;
 
 import javax.swing.JOptionPane;
 
--- RentaCar/src/rentacar/Interfaz_ListarVehiculos.java
+++ RentaCar/src/rentacar/Interfaz_ListarVehiculos.java
@@ -32,16 +32,8 @@ public class Interfaz_ListarVehiculos extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        listarVehiculos = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
-
-        listarVehiculos.setText("Listar vehiculos");
-        listarVehiculos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                listarVehiculosActionPerformed(evt);
-            }
-        });
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
@@ -51,10 +43,6 @@ public class Interfaz_ListarVehiculos extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(listarVehiculos, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(193, 193, 193))
             .addGroup(layout.createSequentialGroup()
                 .addGap(23, 23, 23)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 519, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -65,27 +53,15 @@ public class Interfaz_ListarVehiculos extends javax.swing.JInternalFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
-                .addComponent(listarVehiculos, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(50, 50, 50))
+                .addContainerGap(123, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void listarVehiculosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listarVehiculosActionPerformed
-        try {
-            listarVehiculos();
-        } catch (SQLException ex) {
-            System.out.println("Se ha producido un error inesperado.");
-            System.out.println(ex.getMessage());
-            System.out.println(ex.getCause());        }
-    }//GEN-LAST:event_listarVehiculosActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JButton listarVehiculos;
     // End of variables declaration//GEN-END:variables
 }
